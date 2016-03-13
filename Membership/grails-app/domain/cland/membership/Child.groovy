fe91d5a85beefeabc3a80853df6cbb5034c649f2
Lungelo <Lungelo@Lenovo-PC>
2016-03-13 21:16:25 +0200

    Added remaining fields for child domain, controller, views [accessNumber, comments, ,medicalComments]

--- Membership/grails-app/domain/cland/membership/Child.groovy
+++ Membership/grails-app/domain/cland/membership/Child.groovy
@@ -4,8 +4,15 @@ import cland.membership.security.Person
 
 class Child {
 	Person person
+	BigInteger accessNumber
+	String comments
+	String medicalComments
+	
 	static hasMany = [visits:Visit]
 	static belongsTo = [parent:Parent]
     static constraints = {
+		accessNumber unique: true
+		comments nullable: true
+		medicalComments nullable: true
     }
 }
