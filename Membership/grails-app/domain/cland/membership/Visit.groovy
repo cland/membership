61919c2357e8442f967c7ff82c9f52ffc9f3e685
Tagumi <tagumi.solutions@gmail.com>
2016-03-19 16:10:06 +0200

    added some attributes to the domains

--- Membership/grails-app/domain/cland/membership/Visit.groovy
+++ Membership/grails-app/domain/cland/membership/Visit.groovy
@@ -1,8 +1,67 @@
 package cland.membership
 
+import cland.membership.security.Person
+import java.util.Date;
+
+import org.joda.time.DateTime
+
 class Visit {
-	
+	transient cbcApiService
+	transient springSecurityService
+	transient groupManagerService
+	Date visitDate
+	DateTime starttime
+	DateTime endtime
+	String status
 	static belongsTo = [child:Child]
+	long createdBy
+	long lastUpdatedBy
+	Date dateCreated
+	Date lastUpdated
     static constraints = {
+		lastUpdatedBy nullable:true, editable:false
+		createdBy nullable:true, editable:false
     }
-}
+	def beforeInsert() {
+		encodePassword()
+		long curId = groupManagerService.getCurrentUserId()
+		createdBy = curId
+		lastUpdatedBy = curId		
+	}
+
+	def beforeUpdate() {
+		if (isDirty('password')) {
+			encodePassword()
+		}
+		lastUpdatedBy = groupManagerService.getCurrentUserId()
+	}
+	def toMap(params = null){
+		return [id:id,
+			child:child,
+			date:visitDate?.format("dd-MMM-yyyy"),
+			starttime:starttime,
+			endtime:endtime,
+			status:status,
+			createdbyname:getCreatedByName(),
+			lastupdatedbyname:getLastUpdatedByName(),
+			params:params]
+	}
+	def toAutoCompleteMap(){		
+		return [id:id,
+		label:visitDate?.format("dd-MMM-yyyy") + " " + starttime + " to " + endtime + " | " + status,
+		value:id,
+		child:(this==null?Child.get(id):this),
+		category:(status==null?"Unknown":status)]
+	}
+	String toString(){
+		return starttime + " - " + endtime
+	}
+	String getCreatedByName(){
+		Person user = Person.get(createdBy)
+		return (user==null?"unknown":user?.toString())
+	}
+	String getLastUpdatedByName(){
+		Person user = Person.get(lastUpdatedBy)
+		return (user==null?"unknown":user?.toString())
+	}
+} //end class
